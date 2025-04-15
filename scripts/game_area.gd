1d868d33cefb2e601629f2756571a54ee09a7479
Toggleableable <92555610+Toggleableable@users.noreply.github.com>
2025-04-15 14:12:21 +0100

    Added function to shuffle the next 7 pieces and add them to the queue

--- scripts/game_area.gd
+++ scripts/game_area.gd
@@ -2,20 +2,30 @@ extends TileMapLayer
 
 const rows: int = 22
 const columns: int = 10
+const start_location: Vector2i = Vector2i(2,0)
 
 var pieces: Array
+var next_pieces: Array = []
+var shuffle_array: Array
 var tileset_id: int = 0
 
 var current_rotation: int = 0
+var current_location: Vector2i = start_location
 
 func _ready():
-	var location: Vector2i = Vector2i(0,0)
 	pieces = $Shape.get_children()
-	for i in pieces:
-		draw_piece(i, location)
-		location += Vector2i(0,2)
+	shuffle_array = range(pieces.size())
+	shuffle_pieces()
+	
+	for i in next_pieces:
+		draw_piece(pieces[i], current_location)
+		current_location += Vector2i(2,2)
 
 ## Iterates through the cells in a piece and draws them in the given location
 func draw_piece(current_piece: Shape, location: Vector2i):
-	for i in current_piece.piece_shapes[current_piece.current_rotation]:
+	for i in current_piece.piece_shapes[current_rotation]:
 		set_cell(i + location, tileset_id, Vector2i(current_piece.colour_index, 0))
+
+func shuffle_pieces():
+	shuffle_array.shuffle()
+	next_pieces.append_array(shuffle_array)
