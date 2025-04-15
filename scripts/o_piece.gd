e98f080291d24d148004aa97a74b8422746cc2b4
Toggleableable <92555610+Toggleableable@users.noreply.github.com>
2025-04-15 12:54:09 +0100

    Updated rotate function so it's no longer being passed an attribute it already has access to

--- scripts/o_piece.gd
+++ scripts/o_piece.gd
@@ -4,6 +4,6 @@ func _ready():
 	piece_shapes = [[Vector2i(1,0), Vector2i(2,0), Vector2i(1,1), Vector2i(2,1)]]
 	colour_index = 1
 
-func rotate_piece(current_rotation, direction):
+func rotate_piece(_direction):
 	# O piece doesn't rotate
 	pass
--- scripts/shape.gd
+++ scripts/shape.gd
@@ -6,6 +6,6 @@ var piece_shapes: Array
 var current_rotation: int = 0
 var colour_index: int
 
-func rotate_piece(current_rotation, direction):
+func rotate_piece(direction):
 	# direction must be 1 for clockwise, -1 for counter clockwise
 	current_rotation = (current_rotation + direction) % 4
