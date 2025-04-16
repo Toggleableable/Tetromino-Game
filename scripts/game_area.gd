64b2b75811f2ce903ca82e3046bc025845ff8b5a
Toggleableable <92555610+Toggleableable@users.noreply.github.com>
2025-04-16 18:46:28 +0100

    Added more comments to explain functions

--- scripts/game_area.gd
+++ scripts/game_area.gd
@@ -5,6 +5,8 @@ const columns: int = 10
 const start_location: Vector2i = Vector2i(3,-1)
 const start_rotation: int = 0
 const next_queue: int = 5
+const place_time: float = 0.5
+const place_reset_limit: int = 15
 
 var fall_time: float
 var pieces: Array
@@ -12,6 +14,7 @@ var next_pieces: Array = []
 var next_piece: int
 var shuffle_array: Array
 var tileset_id: int = 0
+var place_resets: int = 0
 
 var current_rotation: int = 0
 var current_location: Vector2i = start_location
@@ -22,9 +25,7 @@ func _ready():
 	shuffle_array = range(pieces.size())
 	
 	# New Game
-	shuffle_pieces()
 	create_piece()
-	$DropPieceTimer.start(fall_time)
 
 func _process(_delta):
 	# TODO change to be based on time, implement DAS and ARR
@@ -41,6 +42,7 @@ func _process(_delta):
 	if Input.is_action_just_pressed("rotate_counter_clockwise"):
 		current_rotation = current_piece.rotate_piece(current_rotation, -1)
 
+## Checks if the current piece is able to move in the direction given
 func can_move(direction: Vector2i) -> bool:
 	var used_cells: Array = $PlacedPieces.get_used_cells()
 	for i in current_piece.piece_shapes[current_rotation]:
@@ -49,16 +51,19 @@ func can_move(direction: Vector2i) -> bool:
 			return false
 		if piece_location in used_cells:
 			return false
-	
 	return true
 
+## Clears the tiles of the current piece in the current location
 func clear_piece():
 	for i in current_piece.piece_shapes[current_rotation]:
 		erase_cell(i + current_location)
 
+## Generates a new piece from the next_pieces queue and reset all the relavent variables
 func create_piece():
 	current_rotation = start_rotation
 	current_location = start_location
+	place_resets = 0
+	#$DropPieceTimer.start(fall_time)
 	if next_pieces.size() < next_queue:
 		shuffle_pieces()
 	current_piece = pieces[next_pieces.pop_front()]
@@ -70,6 +75,7 @@ func draw_piece():
 	for i in current_piece.piece_shapes[current_rotation]:
 		set_cell(i + current_location, tileset_id, Vector2i(current_piece.colour_index, 0))
 
+## Moves the current piece in the direction specified
 func move_piece(direction: Vector2i):
 	if can_move(direction):
 		clear_piece()
@@ -78,6 +84,7 @@ func move_piece(direction: Vector2i):
 	elif direction == Vector2i.DOWN:
 		place_piece()
 
+## Places the piece onto the placed layer and creates the next piece
 func place_piece():
 	clear_piece()
 	for i in current_piece.piece_shapes[current_rotation]:
